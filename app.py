5fbf84ea210556557a0ef60c5006dedc002074f1
Gilbert Keithline <keithlig@classwork.engr.oregonstate.edu>
2024-12-02 14:02:10 -0800

    players is up!

--- app.py
+++ app.py
@@ -168,6 +168,111 @@ def editTeam(id):
                 mysql.connection.commit()
                 
         return redirect("/teams")
+    
+@app.route("/injuryStatus", methods=["POST", "GET"])
+# 'Display' Users table and enable 'Create' functionality
+def injury():
+    # Render the table (Display)
+    if request.method == 'GET':
+        query = "SELECT injury_id as ID, injury_tag as Tag, injury_description as 'Status Description' FROM Injury_Status;"
+        cursor = mysql.connection.cursor()
+        cursor.execute(query)
+        injury_data = cursor.fetchall()
+
+        return render_template('injuryStatus.j2', injury_data = injury_data)
+    
+    # Add a new user (Create)
+    if request.method == 'POST':
+        if request.form.get("addInjury"):
+            tag = request.form["injury_tag"]
+            desc = request.form["injury_description"]
+
+            query = "INSERT INTO Injury_Status (injury_tag, injury_description) VALUES (%s, %s)"
+            cursor = mysql.connection.cursor()
+            cursor.execute(query, (tag, desc))
+            mysql.connection.commit()
+        
+        return redirect("/injuryStatus")
+    
+@app.route("/removeInjury/<int:id>")
+# 'Remove' a table item
+def removeInjury(id):
+    # Deletes a User (Remove)
+    query = "DELETE FROM Injury_Status WHERE injury_id = '%s';"
+    cursor = mysql.connection.cursor()
+    cursor.execute(query, (id,))
+    mysql.connection.commit()
+
+    return redirect("/injuryStatus")
+
+@app.route("/editInjury/<int:id>", methods=["POST", "GET"])
+# Enables 'Update' funtionality for User items
+def editInjury(id):
+    # Renders the update template
+    if request.method == "GET":
+        query = 'SELECT * FROM Injury_Status WHERE injury_id =%s' % (id)
+        cursor = mysql.connection.cursor()
+        cursor.execute(query)
+        injury_data = cursor.fetchall()
+
+        return render_template("editInjury.j2", injury_data=injury_data)
+    
+    # Allows database user to change row information based on user_id selection (Update)
+    if request.method == "POST":
+        if request.form.get("editInjury"):
+                id = request.form["injury_id"]
+                tag = request.form["injury_tag"]
+                desc = request.form["injury_description"]
+
+                query = "UPDATE Injury_Status SET Injury_Status.injury_tag = %s, Injury_Status.injury_description = %s WHERE Injury_Status.injury_id = %s"
+                cursor = mysql.connection.cursor()
+                cursor.execute(query, (tag, desc, id))
+                mysql.connection.commit()
+                
+        return redirect("/injuryStatus")
+    
+
+@app.route("/players", methods=["POST", "GET"])
+# 'Display' Users table and enable 'Create' functionality
+def players():
+    # Render the table (Display)
+    if request.method == 'GET':
+        query = "SELECT player_id as ID, player_name as 'Player Name', point_stat as Points, assist_stat as Assists, rebound_stat as Rebounds, block_stat as Blocks, steal_stat as Steals, timePlayed_stat as Minutes, team_id as 'Team ID', injury_id as 'Injury Status' FROM Players;"
+        cursor = mysql.connection.cursor()
+        cursor.execute(query)
+        player_data = cursor.fetchall()
+
+        query2 = "SELECT team_id FROM Teams"
+        cur = mysql.connection.cursor()
+        cur.execute(query2)
+        team_data = cur.fetchall()
+
+        query3 = "SELECT injury_tag FROM Injury_Status"
+        cur = mysql.connection.cursor()
+        cur.execute(query3)
+        injury_data = cur.fetchall()
+
+        return render_template('players.j2', player_data = player_data, team_data = team_data, injury_data = injury_data)
+    
+    # Add a new user (Create)
+    if request.method == 'POST':
+        if request.form.get("addPlayer"):
+            name = request.form["player_name"]
+            points = request.form["player_points"]
+            assists = request.form["player_assists"]
+            rebounds = request.form["player_rebounds"]
+            blocks = request.form["player_blocks"]
+            steals = request.form["player_steals"]
+            minutes = request.form["player_minutes"]
+            team = request.form["player_team"]
+            injury = request.form["player_injury"]
+
+            query4 = "INSERT INTO Players (player_name, point_stat, assist_stat, rebound_stat, block_stat, steal_stat, timePlayed_stat, team_id, injury_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
+            cursor = mysql.connection.cursor()
+            cursor.execute(query4, (name, points, assists, rebounds, blocks, steals, minutes, team, injury))
+            mysql.connection.commit()
+        
+        return redirect("/players")
 
 
 # Listener
