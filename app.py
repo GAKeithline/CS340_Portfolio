56cc09623aa58770b29eb984c26642e202614f00
Gilbert Keithline <keithlig@classwork.engr.oregonstate.edu>
2024-12-09 21:31:41 -0800

    scrubbed private data

--- app.py
+++ app.py
@@ -33,9 +33,9 @@ app = Flask(__name__)
 
 # database connection info
 app.config["MYSQL_HOST"] = "classmysql.engr.oregonstate.edu"
-app.config["MYSQL_USER"] = "cs340_keithlig"
-app.config["MYSQL_PASSWORD"] = "2334"
-app.config["MYSQL_DB"] = "cs340_keithlig"
+app.config["MYSQL_USER"] = "username"
+app.config["MYSQL_PASSWORD"] = "XXXX"
+app.config["MYSQL_DB"] = "username"
 app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
